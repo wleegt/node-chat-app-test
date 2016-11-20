81d4523ea6b672539f4e108f8ec680fc5288c893
Wook Lee <wleegt@gmail.com>
2016-11-20 21:27:59 +0900

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Mr.Server',
-    text: 'Whatsup, client'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Ms.Client',
-    text: 'Hey how are you, Server',
-    createdAt: 123342
-  });
-
   socket.on('createMessage', function(message) {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', function() {
